5e2c6c82f9cce27a82e089fa5830eb3748835ea8
Sathyanarayan09 <sathyanarayan065@gmail.com>
2017-02-06 19:20:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,3 @@
-console.log('Loaded!');
 /*hier me text*/
 
 var TxtType = function(el, toRotate, period) {
@@ -51,3 +50,203 @@ var TxtType = function(el, toRotate, period) {
               new TxtType(elements[i], JSON.parse(toRotate), period);
             }
         }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #000000}";
+        document.body.appendChild(css);
+    };
+    
+  function loadLoginForm () {
+    var loginHtml = `
+  <h3>Login/Register Here for Posting Comments on the Articles!</h3>
+        <div class="row control-group">
+            <div class="form-group col-xs-12 floating-label-form-group controls">
+              <label>Username</label>
+                <input type="text" class="form-control" id="username" placeholder="Enter Your Username" required>
+            </div>
+        </div>
+        <div class="row control-group">
+            <div class="form-group col-xs-12 floating-label-form-group controls">
+              <label>Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Password" required>
+            </div>
+        </div>
+        <br/>
+        <input type="submit" class="btn btn-default" id="login_btn" value="Login" />
+        <input type="submit" class="btn btn-default" id="register_btn" value="Register" />
+        `;
+    document.getElementById('login_area').innerHTML = loginHtml;
+    
+    // Submit username/password to login
+    var submit = document.getElementById('login_btn');
+    submit.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  submit.value = 'Sucess!';
+              } else if (request.status === 403) {
+                  submit.value = 'Invalid credentials. Try again?';
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              } else {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              }
+              loadLogin();
+          }  
+          // Not done yet
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        submit.value = 'Logging in...';
+        
+    };
+    
+    var register = document.getElementById('register_btn');
+    register.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  alert('User created successfully');
+                  register.value = 'Registered!';
+              } else {
+                  alert('Could not register the user');
+                  register.value = 'Register';
+              }
+          }
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/create-user', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        register.value = 'Registering...';
+    
+    };
+}
+
+function loadLoggedInUser (username) {
+    var loginArea = document.getElementById('login_area');
+    loginArea.innerHTML = `
+        <h3> Hi <i>${username}</i></h3><a href="/logout"> <p align="right">Logout </p></a> 
+        
+  
+    `;
+    
+}
+
+function loadLoggedInUser (username) {
+    var loginArea = document.getElementById('login_area');
+    loginArea.innerHTML = `
+       <h3> Hi <i>${username}</i></h3><a href="/logout"> <p align="right">Logout </p></a> 
+        
+        <h1 align="center"> Publish Your Artical</h1>
+        <div class="row control-group">
+            <div class="form-group col-xs-12 floating-label-form-group controls">
+              <label>Author Name</label>
+                <input type="text" class="form-control" id="author" placeholder="Type.." required>
+            </div></div>
+       <div class="row control-group">
+            <div class="form-group col-xs-12 floating-label-form-group controls">
+              <label>Title</label>
+                <input type="text" class="form-control" id="title" placeholder="Type.." required>
+            </div>
+        </div>
+        <div class="row control-group">
+            <div class="form-group col-xs-12 floating-label-form-group controls">
+              <label>Content</label>
+                <textarea class="form-control" rows="5" placeholder="Type.." id="content"></textarea>
+            </div>
+        </div>
+        
+        <input type="submit" class="btn btn-default" id="sum" value="Submit" />
+        <a href="article.html" class="btn btn-info" role="button">View Articles</a>
+    `;
+    
+       var art = document.getElementById('sum');
+    art.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  alert('Article successfully posted');
+                  art.value = 'Posted succesful!';
+                  this.setHeader('Location', '/');
+              } else {
+                  alert('Could not able to post! try again');
+                  
+              }
+          }
+        };
+        
+        // Make the request
+        var title = document.getElementById('title').value;
+        var content = document.getElementById('content').value;
+         var  author = document.getElementById('author').value;
+    
+       console.log(title);
+        console.log(content);
+          if (title == '' || content == '' || author == '') {
+        // Inform the user on the screen through some message or give him a alert message
+         alert('Why is that field empty?!');
+       
+    }
+       
+        request.open('POST', '/article-post', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({title: title, content: content, author: author}));  
+        register.value = 'Posting...';
+    
+    };
+}
+
+
+
+
+function loadLogin () {
+    // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                loadLoggedInUser(this.responseText);
+            } else {
+                loadLoginForm();
+            }
+        }
+    };
+    
+    request.open('GET', '/check-login', true);
+    request.send(null);
+}
+
+
+// The first thing to do is to check if the user is logged in!
+loadLogin();
